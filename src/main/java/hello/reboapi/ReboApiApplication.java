fd346c1f8f387bebb4d5b0de600bf921f4c4796a
kittae <jinkitea@naver.com>
2025-05-14 00:55:50 +0900

    chore: 이슈, pr템플릿 설정 추가

---
+++ src/main/java/hello/reboapi/ReboApiApplication.java
@@ -0,0 +1,13 @@
+package hello.reboapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReboApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReboApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/reboapi/ReboApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.reboapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReboApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
