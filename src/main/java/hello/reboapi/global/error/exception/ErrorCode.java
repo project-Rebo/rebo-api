ca5c3e25823ab42842f4638d0d7f4b9b7408fa0f
kittae <jinkitea@naver.com>
2025-05-18 22:42:35 +0900

    feat: kakao errorEnum 추가

--- src/main/java/hello/reboapi/global/error/exception/ErrorCode.java
+++ src/main/java/hello/reboapi/global/error/exception/ErrorCode.java
@@ -12,7 +12,10 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "사용자를 찾을 수 없습니다."),
     USER_LIST_EMPTY(HttpStatus.NOT_FOUND, "USER_404", "조회할 사용자가 없습니다."),
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER_409", "이미 존재하는 사용자입니다."),
-    INVALID_USER_INPUT(HttpStatus.BAD_REQUEST, "USER_400", "사용자 입력값이 올바르지 않습니다.");
+    INVALID_USER_INPUT(HttpStatus.BAD_REQUEST, "USER_400", "사용자 입력값이 올바르지 않습니다."),
+    // USER
+    KAKAO_NOT_FOUND(HttpStatus.NOT_FOUND, "KAKAO_404", "카테고리를 찾을 수 없습니다."),
+    INVALID_KAKAO_INPUT(HttpStatus.BAD_REQUEST, "KAKAO_400", "사용자 입력값이 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
